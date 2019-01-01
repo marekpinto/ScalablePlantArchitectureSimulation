c9543255bcc5a2c0eafe76beb6a7f4519c92c7ca
Marek Pinto <marek.pinto@gmail.com>
2019-01-01 12:24:12 -0800

    Update treeData.c

--- treeData.c
+++ treeData.c
@@ -7,23 +7,36 @@ struct node{
 	int length;
 	struct node *left;
 	struct node *right;
+	mat4x4 transformation;
 };
 
-struct node* newNode(int area, int mass, int length){
-	struct node* node = (struct node*)malloc(sizeof(struct node));
-	node->area = area;
-	node->mass = mass;
-	node->length = length;
-	node->left = NULL;
-	node->right = NULL;
-	return (node);
+void newNode(struct node* Node, int area, int mass, int length, mat4x4 transformation){
+	Node->area = area;
+	Node->mass = mass;
+	Node->length = length;
+	Node->left = NULL;
+	Node->right = NULL;
+	mat4x4_dup(Node->transformation,  transformation);	
 }
 
 int main(){
-	struct node *root = newNode(8, 2, 3);
-	root->left = newNode(4, 2, 8);
-	root->right = newNode(6,3 , 3);
+	mat4x4 rootTransformation, transformation1, transformation2, transformation3;
+	newNode(struct node* root, 8, 2, 3, rootTransformation);
+	newNode(root->left, 4, 2, 8, transformation1);
+	newNode(root->right, 6,3 , 3, transformation2);
 
-	root->left->left = newNode(42, 18, 34);
+	newNode(root->left->left, 42, 18, 34, transformation3);
 
+}
+
+void breakOffNode(struct node* Node, int nodeToBreak, struct node* newRoot){
+//if nodeToBreak = 0, delete left child, if 1 delete right child
+	if(nodeToBreak==0){
+		newRoot = Node->left;
+		Node->left = NULL;
+	}
+	else if(nodeToBreak==1){
+		newRoot = Node->right;
+		Node->right = NULL;
+	}
 }
